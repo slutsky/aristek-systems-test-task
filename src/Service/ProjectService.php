e638a1c2e397e8ec51e807b329f976bbd8519e8c
slutsky <slutsky6@gmail.com>
2020-07-31 03:20:18 +0300

    Implement project service

---
+++ src/Service/ProjectService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace AristekSystems\TestTask\Service;
+
+use AristekSystems\TestTask\Dto\CreateContactRequestDto;
+use AristekSystems\TestTask\Dto\CreateProjectRequestDto;
+use AristekSystems\TestTask\Dto\RenameProjectRequestDto;
+use AristekSystems\TestTask\Entity\Contact;
+use AristekSystems\TestTask\Entity\Project;
+use AristekSystems\TestTask\Entity\ProjectId;
+use AristekSystems\TestTask\Exception\ProjectNotFoundException;
+use AristekSystems\TestTask\Repository\ProjectRepositoryInterface;
+use AristekSystems\TestTask\Validator\ProjectValidator;
+
+class ProjectService
+{
+    /** @var ProjectRepositoryInterface */
+    private $projectRepository;
+
+    /** @var ProjectValidator */
+    private $projectValidator;
+
+    /**
+     * @param ProjectRepositoryInterface $projectRepository
+     */
+    public function __construct(
+        ProjectRepositoryInterface $projectRepository,
+        ProjectValidator $projectValidator
+    ) {
+        $this->projectRepository = $projectRepository;
+        $this->projectValidator = $projectValidator;
+    }
+
+    /**
+     * @return Project[]
+     */
+    public function getProjects(): array
+    {
+        return $this->projectRepository->getProjects();
+    }
+
+    /**
+     * @param ProjectId $projectId
+     * @throws ProjectNotFoundException
+     * @return Project
+     */
+    public function getProject(ProjectId $projectId): Project
+    {
+        return $this->projectRepository->getProject($projectId);
+    }
+
+    /**
+     * @param CreateProjectRequestDto $createProjectRequest
+     * @return Project
+     */
+    public function createProject(CreateProjectRequestDto $createProjectRequest): Project
+    {
+        $this->projectValidator->validateCreateProjectRequest($createProjectRequest);
+
+        $project = new Project(
+            $createProjectRequest->getName(),
+            $createProjectRequest->getCode(),
+            $createProjectRequest->getUrl(),
+            $createProjectRequest->getBudget(),
+            array_map(fn (CreateContactRequestDto $createContactRequestDto) => new Contact(
+                $createContactRequestDto->getFirstName(),
+                $createContactRequestDto->getLastName(),
+                $createContactRequestDto->getPhone()
+            ), $createProjectRequest->getContacts())
+        );
+    
+        $this->projectRepository->addProject($project);
+
+        return $project;
+    }
+
+    /**
+     * @param ProjectId $projectId
+     * @throws ProjectNotFoundException
+     */
+    public function deleteProject(ProjectId $projectId): void
+    {
+        $this->projectRepository->removeProject($projectId);
+    }
+
+    /**
+     * @param RenameProjectRequestDto $renameProjectRequest
+     * @throws ProjectNotFoundException
+     * @return Project
+     */
+    public function renameProject(RenameProjectRequestDto $renameProjectRequest): Project
+    {
+        $this->projectValidator->validateRenameProjectRequest($renameProjectRequest);
+
+        $project = $this->projectRepository->getProject($renameProjectRequest->getProjectId());
+        $project->rename($renameProjectRequest->getName());
+
+        return $project;
+    }
+}
---
+++ tests/Service/ProjectServiceTest.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace AristekSystems\TestTask\Tests\Service;
+
+use AristekSystems\TestTask\Dto\CreateContactRequestDto;
+use AristekSystems\TestTask\Dto\CreateProjectRequestDto;
+use AristekSystems\TestTask\Dto\RenameProjectRequestDto;
+use AristekSystems\TestTask\Entity\Project;
+use AristekSystems\TestTask\Entity\ProjectId;
+use AristekSystems\TestTask\Repository\ProjectInMemoryRepository;
+use AristekSystems\TestTask\Repository\ProjectRepositoryInterface;
+use AristekSystems\TestTask\Service\ProjectService;
+use AristekSystems\TestTask\Validator\ProjectValidator;
+use PHPUnit\Framework\TestCase;
+
+class ProjectServiceTest extends TestCase
+{
+    /**
+     * @param ProjectId $projectId
+     * @return Project
+     */
+    private function createProjectStub(ProjectId $projectId): Project
+    {
+        $project = $this->createStub(Project::class);
+        $project->method('getId')->willReturn($projectId);
+
+        /** @var Project $project */
+        return $project;
+    }
+
+    public function testGetProjects(): void
+    {
+        $expectedProject = $this->createProjectStub(new ProjectId(1));
+        $projectRepository = new ProjectInMemoryRepository([$expectedProject]);
+        $projectValidator = $this->createMock(ProjectValidator::class);
+        /** @var ProjectValidator $projectValidator */
+
+        $projectService = new ProjectService($projectRepository, $projectValidator);
+        
+        $projects = $projectService->getProjects();
+        $this->assertCount(1, $projects);
+
+        $project = $projects[0];
+        $this->assertEquals($expectedProject, $project);
+    }
+
+    public function testGetProject(): void
+    {
+        $expectedProject =  $this->createProjectStub(new ProjectId(2));
+        $projectRepository = new ProjectInMemoryRepository([
+            $this->createProjectStub(new ProjectId(1)),
+            $expectedProject, 
+            $this->createProjectStub(new ProjectId(3)),
+        ]);
+        $projectValidator = $this->createMock(ProjectValidator::class);
+        /** @var ProjectValidator $projectValidator */
+
+        $projectService = new ProjectService($projectRepository, $projectValidator);
+        
+        $project = $projectService->getProject(new ProjectId(2));
+        $this->assertEquals($expectedProject, $project);
+    }
+
+    public function testCreateProject(): void
+    {
+        $expectedName = 'Project';
+        $expectedCode = 'np';
+        $expectedUrl = 'http://example.com/new-page';
+        $expectedBudget = 100;
+        $expectedContactFirstName = 'Jon';
+        $expectedContactLastName = 'Doe';
+        $expectedContactPhone = '+012 (34) 567-89-10';
+
+        $projectValidator = $this->createMock(ProjectValidator::class);
+        $projectValidator->expects($this->once())->method('validateCreateProjectRequest');
+        /** @var ProjectValidator $projectValidator */
+
+        $projectRepository = $this->createMock(ProjectRepositoryInterface::class);
+        $projectRepository->expects($this->once())->method('addProject');
+        /** @var ProjectRepositoryInterface $projectRepository */
+
+        $projectService = new ProjectService($projectRepository, $projectValidator);
+
+        $project = $projectService->createProject(
+            new CreateProjectRequestDto(
+                $expectedName,
+                $expectedCode,
+                $expectedUrl,
+                $expectedBudget,
+                [ 
+                    new CreateContactRequestDto(
+                        $expectedContactFirstName,
+                        $expectedContactLastName,
+                        $expectedContactPhone
+                    )
+                ]
+            )
+        );
+
+        $this->assertEquals($expectedName, $project->getName());
+        $this->assertEquals($expectedCode, $project->getCode());
+        $this->assertEquals($expectedUrl, $project->getUrl());
+        $this->assertEquals($expectedBudget, $project->getBudget());
+
+        $contacts = $project->getContacts();
+        $this->assertCount(1, $contacts);
+
+        $contact = $contacts[0];
+        $this->assertEquals($expectedContactFirstName, $contact->getFirstName());
+        $this->assertEquals($expectedContactLastName, $contact->getLastName());
+        $this->assertEquals($expectedContactPhone, $contact->getPhone());
+    }
+
+    public function testDeleteProject(): void
+    {
+        $projectRepository = $this->createMock(ProjectRepositoryInterface::class);
+        $projectRepository->expects($this->once())->method('removeProject');
+        /** @var ProjectRepositoryInterface $projectRepository */
+
+        $projectValidator = $this->createMock(ProjectValidator::class);
+        /** @var ProjectValidator $projectValidator */
+
+        $projectService = new ProjectService($projectRepository, $projectValidator);
+
+        $projectService->deleteProject(new ProjectId(1));
+    }
+
+    public function testRenameProject(): void
+    {
+        $project = $this->createMock(Project::class);
+        $project->method('getId')->willReturn(new ProjectId(1));
+        $project->expects($this->once())->method('rename');
+        /** @var Project $project */
+
+        $projectRepository = new ProjectInMemoryRepository([$project]);
+
+        $projectValidator = $this->createMock(ProjectValidator::class);
+        $projectValidator->expects($this->once())->method('validateRenameProjectRequest');
+        /** @var ProjectValidator $projectValidator */
+
+        $projectService = new ProjectService($projectRepository, $projectValidator);
+
+        $projectService->renameProject(
+            new RenameProjectRequestDto(new ProjectId(1), 'New name')
+        );
+    }
+}
